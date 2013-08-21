8779c3ab98e44df18e964dcb05fa562c69d7eebb
geekwright <richard@geekwright.com>
2013-08-20 22:47:03 -0500

    Rework Metagen to provide simpler static methods.
    Also add stopwords language files, to help in picking keywords from an 
    arbitray body of text. Additional languages are available, but not yet 
    converted from Lingua-Stopwords cpan module.

--- libraries/Xmf/Metagen.php
+++ libraries/Xmf/Metagen.php
@@ -12,10 +12,11 @@
 namespace Xmf;
 
 /**
- * Metagen
+ * Metagen facilitates generating and assigning page meta tags
  *
  * @category  Xmf\Module\Metagen
  * @package   Xmf
+ * @author    Richard Griffith <richard@geekwright.com>
  * @author    trabis <lusopoemas@gmail.com>
  * @copyright 2011-2013 The XOOPS Project http://sourceforge.net/projects/xoops/
  * @license   http://www.fsf.org/copyleft/gpl.html GNU public license
@@ -27,78 +28,220 @@ class Metagen
 {
 
     /**
-     * @var string
-     */
-    private $_title;
-
-    /**
-     * @var string
-     */
-    private $_original_title;
-
-    /**
-     * @var string
-     */
-    private $_keywords;
-
-    /**
-     * @var string
-     */
-    private $_meta_description;
-
-    /**
-     * @var string
-     */
-    private $_category_path;
-
-    /**
-     * @var string
-     */
-    private $_description;
-
-    /**
-     * @var int
-     */
-    private $_minChar = 5;
-    private $_maxKeywords = 20;
-
-public static function assignTitle($title) {}
-public static function assignKeywords($keywords) {}
-public static function assignDescription($description) {}
-public static function generateKeywords($body, $forceKeys = null, $count=20, $minLength=5) {}
-public static function generateDescription($body, $wordCount = 100) {}
-public static function generateMetaTags($title, $body, $forceKeys = null, $count=20, $minLength=5, $wordCount = 100) {}
-
-    /**
-     * Constructor
+     * assignTitle set the page title
      *
-     * @param string $title         Page title
-     * @param string $keywords      List of meta keywords
-     * @param string $description   Meta description
-     * @param string $category_path category
+     * @param string $title page title
+     *
+     * @return void
      */
-    public function __construct($title, $keywords = '', $description = '', $category_path = '')
+    public static function assignTitle($title)
     {
-        $this->setCategoryPath($category_path);
-        $this->setTitle($title);
-        $this->setDescription($description);
+        global $xoopsTpl, $xoTheme;
 
-        if (empty($keywords)) {
-            $keywords = $this->createMetaKeywords();
+        $title = trim($title);
+        if (!empty($title)) {
+            $title = Utilities::html2text($title);
+            $title = Utilities::purifyText($title);
+            if (is_object($xoTheme)) {
+                $xoTheme->addMeta('meta', 'title', $title);
+            }
+            $xoopsTpl->assign('xoops_pagetitle', $title);
+        }
+    }
+
+    /**
+     * assignKeywords set the meta keywords tag
+     *
+     * @param array $keywords keywords for page
+     *
+     * @return void
+     */
+    public static function assignKeywords($keywords)
+    {
+        global $xoopsTpl, $xoTheme;
+
+        if (!empty($keywords) && is_array($keywords)) {
+            $keyword_tag = implode(', ', $keywords);
+
+            if (!empty($keyword_tag)) {
+                if (is_object($xoTheme)) {
+                    $xoTheme->addMeta('meta', 'keywords', $keyword_tag);
+                } else {
+                    $xoopsTpl->assign('xoops_meta_keywords', $keyword_tag);
+                }
+            }
+        }
+    }
+
+    /**
+     * assignDescription set the meta description tag
+     *
+     * @param string $description page description
+     *
+     * @return void
+     */
+    public static function assignDescription($description)
+    {
+        global $xoopsTpl, $xoTheme;
+
+        $description = trim($description);
+        if (!empty($description)) {
+            if (is_object($xoTheme)) {
+                $xoTheme->addMeta('meta', 'description', $description);
+            } else {
+                $xoopsTpl->assign('xoops_meta_description', $description);
+            }
+        }
+    }
+
+    /**
+     * generateKeywords builds a set of keywords from text body
+     *
+     * @param string $body      text to extract keywords from
+     * @param mixed  $forceKeys array of keywords to force use, or null for none
+     * @param int    $count     number of keywords to use
+     * @param int    $minLength minimum length of word to consider as a keyword
+     * 
+     * @return array of keywords
+     */
+    public static function generateKeywords(
+        $body, $forceKeys = null, $count=20, $minLength=4
+    ) {
+        $keywords = array();
+
+        $text = trim($body);
+        $text = strtolower($text);
+        $text = Utilities::html2text($text);
+        $text = Utilities::purifyText($text);
+
+        $text = preg_replace("/([^\r\n])\r\n([^\r\n])/", "\\1 \\2", $text);
+        $text = preg_replace("/[\r\n]*\r\n[\r\n]*/", "\r\n\r\n", $text);
+        $text = preg_replace("/[ ]* [ ]*/", ' ', $text);
+        $text = StripSlashes($text);
+
+        $originalKeywords = preg_split(
+            '/[^a-zA-Z\'"-]+/', $text, -1, PREG_SPLIT_NO_EMPTY
+        );
+
+        foreach ($originalKeywords as $originalKeyword) {
+            if (self::checkStopWords($originalKeyword)) {
+                $secondRoundKeywords = explode("'", $originalKeyword);
+                foreach ($secondRoundKeywords as $secondRoundKeyword) {
+                    if (self::checkStopWords($secondRoundKeyword)
+                        && strlen($secondRoundKeyword) >= $minLength
+                    ) {
+                        if (!in_array($secondRoundKeyword, $keywords)) {
+                            $key[$secondRoundKeyword] = $secondRoundKeyword;
+                            if(empty($keycnt[$secondRoundKeyword])) $keycnt[$secondRoundKeyword] = 0;
+                             $keycnt[$secondRoundKeyword] += 1;
+                            //$keywords[] = trim($secondRoundKeyword);
+                        }
+                    }
+                }
+            }
         }
 
-        $this->setKeywords($keywords);
+        array_multisort($keycnt, SORT_DESC, $key, SORT_ASC);
+        while (!empty($forceKeys)) {
+            $tempkey = array_pop($forceKeys);
+            array_unshift($key, $tempkey);
+        }
+
+        $keywords = array_slice($key, 0, $count);
+        //$keywords = $key;
+
+        return $keywords;
+    }
+    
+    /**
+     * checkStopWords - look up a word in a list of stop words and
+     * classify it as a significant word or a stop word.
+     * 
+     * @param string $key the word to check
+     *  
+     * @return bool True if word is significant, false if it is a stop word
+     */
+    protected static function checkStopWords($key)
+    {
+        static $stopwords = null;
+
+        if (!$stopwords) {
+            if (!defined('_XMF_STOPWORDS')) {
+                \Xmf\Language::load('stopwords', 'xmf');
+            }
+            if (defined('_XMF_STOPWORDS')) {
+                $sw = explode(' ', _XMF_STOPWORDS);
+                $stopwords = array_fill_keys($sw, true);
+            } else {
+                $stopwords = array('_'=> true);
+            }
+        }
+        if ($stopwords) {
+            return !isset($stopwords[$key]);
+        }
+        return true;
+    }
+
+    /**
+     * generateDescription - generate a short description from a body of text
+     * 
+     * @param string $body      body text
+     * @param string $wordCount maximum word count for description
+     * 
+     * @return string 
+     */
+    public static function generateDescription($body, $wordCount = 100)
+    {
+        $text = preg_replace("/([^\r\n])\r\n([^\r\n])/", "\\1 \\2", $body);
+        $text = preg_replace("/[\r\n]*\r\n[\r\n]*/", "\r\n\r\n", $text);
+        $text = preg_replace("/[ ]* [ ]*/", ' ', $text);
+        $text = StripSlashes($text);
+        $words = explode(" ", $text);
+
+        // Only keep $maxWords words
+        $newWords = array();
+        $i = 0;
+        while ($i < $wordCount - 1 && $i < count($words)) {
+            $newWords[] = $words[$i];
+            $i++;
+        }
+        $ret = implode(' ', $newWords);
+
+        return $ret;
+    }
+
+    /**
+     * generateMetaTags - generate and assign all meta tags
+     * 
+     * @param string $title     title
+     * @param string $body      body text
+     * @param array  $forceKeys associative array of keywords to force use 
+     * @param int    $count     maximum keywords to use
+     * @param int    $minLength minimum length of word to consider as keyword
+     * @param int    $wordCount maximum word count for description summary
+     *  
+     * @return void
+     */
+    public static function generateMetaTags(
+        $title, $body, $forceKeys = null, $count=20, $minLength=4, $wordCount = 100
+    ) {
+        $keywords = self::generateKeywords($body, $forceKeys, $count, $minLength);
+        $description = self::generateDescription($body, $wordCount);
+        self::assignTitle($title);
+        self::assignKeywords($keywords);
+        self::assignDescription($description);
     }
 
     /**
      * Return true if the string is length > 0
      *
-     * @author psylove
-     *
-     * @var string $var to test
+     * @param string $var to test
+     * 
      * @return boolean
+     *
+     * @author psylove
      */
-    protected function nonEmptyString($var)
+    protected static function nonEmptyString($var)
     {
         return (strlen($var) > 0);
     }
@@ -106,13 +249,14 @@ public static function generateMetaTags($title, $body, $forceKeys = null, $count
     /**
      * Create a title for the short_url field of an article
      *
-     * @author psylove
+     * @param string $title   title of the article
+     * @param bool   $withExt do we add an html extension or not
      *
-     * @var string $title title of the article
-     * @var bool $withExt do we add an html extension or not
      * @return string sort_url for the article
+     * 
+     * @author psylove
      */
-    public function generateSeoTitle($title = '', $withExt = true)
+    public static function generateSeoTitle($title = '', $withExt = true)
     {
         $title = rawurlencode(strtolower($title));
 
@@ -134,7 +278,7 @@ public static function generateMetaTags($title, $body, $forceKeys = null, $count
         $title = preg_replace($pattern, $rep_pat, $title);
 
         $tableau = explode("-", $title);
-        $tableau = array_filter($tableau, array($this, "nonEmptyString"));
+        $tableau = array_filter($tableau, 'self::nonEmptyString');
         $title = implode("-", $tableau);
 
         if (sizeof($title) > 0) {
@@ -148,247 +292,4 @@ public static function generateMetaTags($title, $body, $forceKeys = null, $count
         }
     }
 
-    /**
-     * Sets the title property
-     * @param string $title
-     *
-     */
-    public function setTitle($title)
-    {
-        global $xoopsModule;
-        $this->_title = $this->html2text($title);
-        $this->_title = $this->purifyText($this->_title);
-        $this->_original_title = $this->_title;
-
-        $moduleName = $xoopsModule->getVar('name');
-
-        $titleTag = array();
-
-        if (isset($this->_title) && ($this->_title != '') && (strtoupper($this->_title) != strtoupper($moduleName))) {
-            $titleTag['title'] = $this->_title;
-        }
-
-        if (isset($this->_category_path) && ($this->_category_path != '')) {
-            $titleTag['category'] = $this->_category_path;
-        }
-
-        $ret = isset($titleTag['title']) ? $titleTag['title'] : '';
-
-        if (isset($titleTag['category']) && $titleTag['category'] != '') {
-            if ($ret != '') {
-                $ret .= ' - ';
-            }
-            $ret .= $titleTag['category'];
-        }
-        if (isset($titleTag['module']) && $titleTag['module'] != '') {
-            if ($ret != '') {
-                $ret .= ' - ';
-            }
-            $ret .= $titleTag['module'];
-        }
-        $this->_title = $ret;
-    }
-
-    /**
-     * Sets the keyword property
-     *
-     * @param string $keywords
-     */
-    public function setKeywords($keywords)
-    {
-        $this->_keywords = $keywords;
-    }
-
-    /**
-     * Sets the categoryPath property
-     *
-     * @param string $categoryPath
-     */
-    public function setCategoryPath($categoryPath)
-    {
-        $this->_category_path = $this->html2text($categoryPath);
-    }
-
-    /**
-     * Sets the description property
-     * @param string $description
-     *
-     */
-    public function setDescription($description)
-    {
-        $description = $this->html2text($description);
-        $description = $this->purifyText($description);
-
-        $description = preg_replace("/([^\r\n])\r\n([^\r\n])/", "\\1 \\2", $description);
-        $description = preg_replace("/[\r\n]*\r\n[\r\n]*/", "\r\n\r\n", $description);
-        $description = preg_replace("/[ ]* [ ]*/", ' ', $description);
-        $description = StripSlashes($description);
-
-        $this->_description = $description;
-        $this->_meta_description = $this->createMetaDescription();
-    }
-
-    /**
-     * Cleans the provided text
-     *
-     * @param  string  $text    Text to be cleaned
-     * @param  boolean $keyword Whether the provided string is a keyword, or not
-     * @return string  The purified text
-     */
-    public function purifyText($text, $keyword = false)
-    {
-        return Utilities::purifyText($text, $keyword);
-    }
-
-    /**
-     *
-     * @param $document
-     * @return string Converted text
-     */
-    public function html2text($document)
-    {
-        return Utilities::html2text($document);
-    }
-
-    /**
-     * Creates a meta description
-     * @param  int    $maxWords Maximum number of words for the description
-     * @return string
-     */
-    public function createMetaDescription($maxWords = 100)
-    {
-        $words = explode(" ", $this->_description);
-
-        // Only keep $maxWords words
-        $newWords = array();
-        $i = 0;
-
-        while ($i < $maxWords - 1 && $i < count($words)) {
-            $newWords[] = $words[$i];
-            $i++;
-        }
-        $ret = implode(' ', $newWords);
-
-        return $ret;
-    }
-
-    /**
-     * Generates a list of keywords from the provided text
-     * @param  string $text    Text to parse
-     * @param  int    $minChar Minimum word length for the keywords
-     * @return array  An array of keywords
-     */
-    public function findMetaKeywords($text, $minChar)
-    {
-        $keywords = array();
-
-        $text = strtolower($text);
-        $text = $this->purifyText($text);
-        $text = $this->html2text($text);
-
-        $text = preg_replace("/([^\r\n])\r\n([^\r\n])/", "\\1 \\2", $text);
-        $text = preg_replace("/[\r\n]*\r\n[\r\n]*/", "\r\n\r\n", $text);
-        $text = preg_replace("/[ ]* [ ]*/", ' ', $text);
-        $text = StripSlashes($text);
-
-        $originalKeywords = preg_split('/[^a-zA-Z\'"-]+/', $text, -1, PREG_SPLIT_NO_EMPTY);
-
-        foreach ($originalKeywords as $originalKeyword) {
-            $secondRoundKeywords = explode("'", $originalKeyword);
-            foreach ($secondRoundKeywords as $secondRoundKeyword) {
-                if (strlen($secondRoundKeyword) >= $minChar) {
-                    if (!in_array($secondRoundKeyword, $keywords)) {
-                        $key[$secondRoundKeyword] = $secondRoundKeyword;
-                        if(empty($keycnt[$secondRoundKeyword])) $keycnt[$secondRoundKeyword] = 0;
-                         $keycnt[$secondRoundKeyword] += 1;
-                        //$keywords[] = trim($secondRoundKeyword);
-                    }
-                }
-            }
-        }
-Debug::dump($keycnt);
-        array_multisort($keycnt, SORT_DESC, $key, SORT_ASC);
-Debug::dump($key);
-        $keywords = $key;
-
-        return $keywords;
-    }
-
-    /**
-     * Creates a string of keywords
-     *
-     * @return string
-     */
-    public function createMetaKeywords()
-    {
-        $keywords = $this->findMetaKeywords($this->_original_title . " " . $this->_description, $this->_minChar);
-
-        $newKeywords = array_slice ($keywords , 0, $this->_maxKeywords );
-
-        $ret = implode(', ', $newKeywords);
-
-        return $ret;
-    }
-
-    /**
-     * @param $keywords
-     * @return void
-     */
-    public function addMetaKeywords($keywords)
-    {
-        if (!empty($keywords)) {
-            $this->_keywords = array_merge($keywords, explode(",", $keywords));
-        }
-    }
-
-    /**
-     * Generates keywords, description and title, setting the associated properties
-     */
-    public function buildAutoMetaTags()
-    {
-        $this->_keywords = $this->createMetaKeywords();
-        $this->_meta_description = $this->createMetaDescription();
-    }
-
-    /**
-     * Assigns the meta tags to the template
-     */
-    public function createMetaTags()
-    {
-        global $xoopsTpl, $xoTheme;
-
-        if (is_object($xoTheme)) {
-            $xoTheme->addMeta('meta', 'keywords', $this->_keywords);
-            $xoTheme->addMeta('meta', 'description', $this->_description);
-            $xoTheme->addMeta('meta', 'title', $this->_title);
-        } else {
-            $xoopsTpl->assign('xoops_meta_keywords', $this->_keywords);
-            $xoopsTpl->assign('xoops_meta_description', $this->_description);
-        }
-        $xoopsTpl->assign('xoops_pagetitle', $this->_title);
-    }
-
-    /**
-     * @return string
-     */
-    public function getTitle()
-    {
-        return $this->_title;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->_description;
-    }
-
-    /**
-     * @return string
-     */
-    public function getKeywords()
-    {
-        return $this->_keywords;
-    }
 }
