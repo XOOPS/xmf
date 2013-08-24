f7adcdf9b2247ee7f6f1beda237d3cb1de7f451a
geekwright <richard@geekwright.com>
2013-08-24 11:33:39 -0500

    Change default highlight to <strong>

--- libraries/Xmf/Highlighter.php
+++ libraries/Xmf/Highlighter.php
@@ -43,7 +43,7 @@ class Highlighter
      *
      * @return string highlighted body
      */
-    public static function apply($words, $body, $pre='<b>', $post='</b>')
+    public static function apply($words, $body, $pre='<strong>', $post='</strong>')
     {
         if (!is_array($words)) {
             $words=str_replace('  ', ' ', $words);
