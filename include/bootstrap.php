b7fbb720f3ece877f388100fed1b7523aeaaebd4
geekwright <richard@geekwright.com>
2013-08-28 11:10:26 -0500

    Cleaning up unused and unneeded  inits

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -19,41 +19,41 @@
  */
 
 
-if (defined('XMF_EXEC')) return;
-
 if (!defined('XOOPS_ROOT_PATH')) {
     require dirname(dirname(dirname(dirname(__FILE__)))) . '/mainfile.php';
 }
 
-define('XMF_EXEC', true);
+// DO NOT depend on these in your - they will change or be removed
+if (!defined('XMF_EXEC')) {
+    define('XMF_EXEC', true);
 
-define('XMF_URL',            XOOPS_URL . '/modules/xmf');
-define('XMF_CSS_URL',        XMF_URL . '/css');
-// define('XMF_IMAGES_URL',     XMF_URL . '/images');
-define('XMF_INCLUDE_URL',    XMF_URL . '/include');
-define('XMF_LANGUAGE_URL',   XMF_URL . '/language');
-define('XMF_LIBRARIES_URL',  XMF_URL . '/libraries');
-define('XMF_TEMPLATES_URL',  XMF_URL . '/templates');
-define('XMF_KRUMO_URL',      XMF_URL . '/css/krumo/');
+    define('XMF_URL', XOOPS_URL . '/modules/xmf');
+    define('XMF_CSS_URL', XMF_URL . '/css');
+    // define('XMF_IMAGES_URL', XMF_URL . '/images');
+    define('XMF_INCLUDE_URL', XMF_URL . '/include');
+    define('XMF_LANGUAGE_URL', XMF_URL . '/language');
+    define('XMF_LIBRARIES_URL', XMF_URL . '/libraries');
+    define('XMF_TEMPLATES_URL', XMF_URL . '/templates');
+    define('XMF_KRUMO_URL', XMF_URL . '/css/krumo/');
 
-define('XMF_ROOT_PATH',      XOOPS_ROOT_PATH . '/modules/xmf');
-define('XMF_CSS_PATH',       XMF_ROOT_PATH . '/css');
-define('XMF_IMAGES_PATH',    XMF_ROOT_PATH . '/images');
-define('XMF_INCLUDE_PATH',   XMF_ROOT_PATH . '/include');
-define('XMF_LANGUAGE_PATH',  XMF_ROOT_PATH . '/language');
-define('XMF_LIBRARIES_PATH', XMF_ROOT_PATH . '/libraries');
-define('XMF_TEMPLATES_PATH', XMF_ROOT_PATH . '/templates');
+    define('XMF_ROOT_PATH', XOOPS_ROOT_PATH . '/modules/xmf');
+    define('XMF_CSS_PATH', XMF_ROOT_PATH . '/css');
+    define('XMF_IMAGES_PATH', XMF_ROOT_PATH . '/images');
+    define('XMF_INCLUDE_PATH', XMF_ROOT_PATH . '/include');
+    define('XMF_LANGUAGE_PATH', XMF_ROOT_PATH . '/language');
+    define('XMF_LIBRARIES_PATH', XMF_ROOT_PATH . '/libraries');
+    define('XMF_TEMPLATES_PATH', XMF_ROOT_PATH . '/templates');
 
-define('XMF_NEWLINE', "\n");
+    define('XMF_NEWLINE', "\n");
 
-define('_GLOBAL_LEFT', 'left');
-define('_GLOBAL_RIGHT', 'right');
-
+    define('_GLOBAL_LEFT', 'left');
+    define('_GLOBAL_RIGHT', 'right');
+}
 //require dirname(dirname(__FILE__)) . '/libraries/Xmf/Loader.php';
 //spl_autoload_register(array('Xmf_Loader', 'loadClass'));
 
-require dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
 // \Xmf\Loader::loadFile(XOOPS_PATH . '/vendor/autoload.php');
 
-require dirname(__FILE__) . '/functions.php';
+//require_once dirname(__FILE__) . '/functions.php';
 \Xmf\Language::load('global', 'xmf');
