ab4f5851e48d73a97e5c0772cda5a9b6bc4b1932
geekwright <richard@geekwright.com>
2013-12-08 16:16:46 -0600

    Add a few stopwords language files and missing index.html in language folders
    The Lingua-Stopwords package contained several languages which were not otherwise included in xmf, but didn't want to waste them. Took the opportunity to add them.

---
+++ language/danish/stopwords.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * DA - Danish - dansk
+ * Adapted from the following under GPL 1 or greater license
+ * http://search.cpan.org/~creamyg/Lingua-StopWords-0.09/
+ */
+define(
+    '_XMF_STOPWORDS',
+    "og i jeg det at en den til er som på de med han af for ikke "
+    . "der var mig sig men et har om vi min havde ham hun nu over da "
+    . "fra du ud sin dem os op man hans hvor eller hvad skal selv her "
+    . "alle vil blev kunne ind når være dog noget ville jo deres "
+    . "efter ned skulle denne end dette mit også under have dig anden "
+    . "hende mine alt meget sit sine vor mod disse hvis din nogle hos "
+    . "blive mange ad bliver hendes været thi jer sådan"
+);
---
+++ language/dutch/stopwords.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * NL - Dutch - Nederlands
+ * Adapted from the following under GPL 1 or greater license
+ * http://search.cpan.org/~creamyg/Lingua-StopWords-0.09/
+ */
+define(
+    '_XMF_STOPWORDS',
+    "de en van ik te dat die in een hij het niet zijn is was op aan "
+    . "met als voor had er maar om hem dan zou of wat mijn men dit zo "
+    . "door over ze zich bij ook tot je mij uit der daar haar naar heb "
+    . "hoe heeft hebben deze u want nog zal me zij nu ge geen omdat "
+    . "iets worden toch al waren veel meer doen toen moet ben zonder "
+    . "kan hun dus alles onder ja eens hier wie werd altijd doch wordt "
+    . "wezen kunnen ons zelf tegen na reeds wil kon niets uw iemand "
+    . "geweest andere"
+);
