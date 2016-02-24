088e530b5054786be32c90e40f7dd7b8905d9796
geekwright <richard@geekwright.com>
2016-02-23 22:30:08 -0600

    Drop Utilities
    Move methods to Metagen, the only consumer, as protected.
    Remove Utilities class.

--- src/Xmf/Metagen.php
+++ src/Xmf/Metagen.php
@@ -404,9 +404,8 @@ class Metagen
     protected static function asPlainText($rawText)
     {
         $text = $rawText;
-        $utilities = new Utilities();
-        $text = $utilities->html2text($text);
-        $text = $utilities->purifyText($text);
+        $text = static::html2text($text);
+        $text = static::purifyText($text);
 
         $text = str_replace(array("\n", "\r"), ' ', $text);
         $text = preg_replace('/[ ]* [ ]*/', ' ', $text);
@@ -439,5 +438,102 @@ class Metagen
             }
         }
         return $pos;
+    }
+
+    /**
+     * purifyText
+     *
+     * @param string  $text    text to clean
+     * @param boolean $keyword replace some punctuation with white space
+     *
+     * @return string cleaned text
+     */
+    protected static function purifyText($text, $keyword = false)
+    {
+        $myts = \MyTextSanitizer::getInstance();
+        $text = str_replace('&nbsp;', ' ', $text);
+        $text = str_replace('<br />', ' ', $text);
+        $text = str_replace('<br/>', ' ', $text);
+        $text = str_replace('<br', ' ', $text);
+        $text = strip_tags($text);
+        $text = html_entity_decode($text);
+        $text = $myts->undoHtmlSpecialChars($text);
+        $text = str_replace(')', ' ', $text);
+        $text = str_replace('(', ' ', $text);
+        $text = str_replace(':', ' ', $text);
+        $text = str_replace('&euro', ' euro ', $text);
+        $text = str_replace('&hellip', '...', $text);
+        $text = str_replace('&rsquo', ' ', $text);
+        $text = str_replace('!', ' ', $text);
+        $text = str_replace('?', ' ', $text);
+        $text = str_replace('"', ' ', $text);
+        $text = str_replace('-', ' ', $text);
+        $text = str_replace('\n', ' ', $text);
+        $text = str_replace('&#8213;', ' ', $text);
+
+        if ($keyword) {
+            $text = str_replace('.', ' ', $text);
+            $text = str_replace(',', ' ', $text);
+            $text = str_replace('\'', ' ', $text);
+        }
+        $text = str_replace(';', ' ', $text);
+
+        return $text;
+    }
+
+    /**
+     * html2text
+     * This will remove HTML tags, javascript sections and white space. It will also
+     * convert some common HTML entities to their text equivalent. Credits to newbb2
+     *
+     * @param string $document HTML to be converted
+     *
+     * @return string Text version of $document parameter
+     */
+    protected static function html2text($document)
+    {
+        $search = array(
+            "'<script[^>]*?>.*?</script>'si", // Strip out javascript
+            "'<img.*?/>'si",                  // Strip out img tags
+            "'<[\/\!]*?[^<>]*?>'si",          // Strip out HTML tags
+            "'([\r\n])[\s]+'",                // Strip out white space
+            "'&(quot|#34);'i",                // Replace HTML entities
+            "'&(amp|#38);'i",
+            "'&(lt|#60);'i",
+            "'&(gt|#62);'i",
+            "'&(nbsp|#160);'i",
+            "'&(iexcl|#161);'i",
+            "'&(cent|#162);'i",
+            "'&(pound|#163);'i",
+            "'&(copy|#169);'i"
+        );
+
+        $replace = array(
+            "",
+            "",
+            "",
+            "\\1",
+            "\"",
+            "&",
+            "<",
+            ">",
+            " ",
+            chr(161),
+            chr(162),
+            chr(163),
+            chr(169)
+        );
+
+        $text = preg_replace($search, $replace, $document);
+
+        preg_replace_callback(
+            '/&#(\d+);/',
+            function ($matches) {
+                return chr($matches[1]);
+            },
+            $document
+        );
+
+        return $text;
     }
 }
