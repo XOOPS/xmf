d2c67f13357e7b8e971e68e7dcad2b297570da55
geekwright <richard@geekwright.com>
2021-03-22 11:03:23 -0500

    Update dates

--- src/Uuid.php
+++ src/Uuid.php
@@ -17,7 +17,7 @@ namespace Xmf;
  * @category  Xmf\Uuid
  * @package   Xmf
  * @author    Richard Griffith <richard@geekwright.com>
- * @copyright 2017-2019 XOOPS Project (https://xoops.org)
+ * @copyright 2017-2021 XOOPS Project (https://xoops.org)
  * @license   GNU GPL 2 or later (https://www.gnu.org/licenses/gpl-2.0.html)
  */
 class Uuid
