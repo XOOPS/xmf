53a5c2cfa3917acfffaa3c10e2f11e32c449b0f4
mambax7 <mambax7@gmail.com>
2023-05-18 23:36:52 -0400

    adding UlidTest

---
+++ tests/unit/UlidTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Xmf\Test;
+
+use Xmf\Ulid;
+
+class UlidTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testGenerate()
+    {
+        $ulid = Ulid::generate();
+
+        $this->assertRegExp('/^[0-9a-f]{26}$/', $ulid);
+    }
+}
+
